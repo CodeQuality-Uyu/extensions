abfac1c62d4ff37abbaa9f51c4eeadfdce88eeed
Daniel Acevedo <149620005+danielAcevedo18@users.noreply.github.com>
2024-08-23 16:47:26 -0300

    Refactor

--- CQ.Extensions.Environments/EnvironmentExtensions.cs
+++ CQ.Extensions.Environments/EnvironmentExtensions.cs
@@ -2,15 +2,44 @@
 using Microsoft.Extensions.Hosting;
 
 namespace CQ.Extensions.Environments;
+
 public static class EnvironmentExtensions
 {
-    public static string Get()
+    private static string Get()
     {
         var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
         return Guard.IsNotNullOrEmpty(environmentName)
-            ? environmentName
+            ? environmentName!
             : "Local";
+    }
+
+    public static bool IsProd()
+    {
+        var environmentName = Get();
+
+        return Guard.Is(environmentName, "Production");
+    }
+
+    public static bool IsLocal()
+    {
+        var environmentName = Get();
+
+        return Guard.Is(environmentName, "Local");
+    }
+
+    public static bool IsTesting()
+    {
+        var environmentName = Get();
+
+        return Guard.Is(environmentName, "Testing");
+    }
+
+    public static bool IsDevelopment()
+    {
+        var environmentName = Get();
+
+        return Guard.Is(environmentName, "Development");
     }
 
     public static bool IsLocal(this IHostEnvironment environment)
